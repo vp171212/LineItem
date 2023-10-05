523577a552fb922efb57999972823a36b138c212
Ved Prakash <91253410+vp171212@users.noreply.github.com>
2023-10-05 21:42:22 +0530

    Add files via upload

---
+++ Model/LineItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineItemList.Model
+{
+    internal class LineItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+        public double ItemCost { get { return UnitPrice*Quantity; } }
+
+       
+        public LineItem (int id,string name,int quantity,double unitPrice)
+        { 
+            Id = id;
+            Name = name; 
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {UnitPrice} X {Quantity} = {ItemCost}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using LineItemList.Model;
+
+namespace LineItemList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<LineItem> items = new List<LineItem>
+            {
+                new LineItem(101,"Milkbikies",2,50),
+                 new LineItem(102,"Dairy Milk",2,100),
+                  new LineItem(103,"Book",3,100)
+            };
+            PrintItems(items);
+        }
+
+        private static void PrintItems(List<LineItem> items)
+        {
+            double total = 0;
+            Console.WriteLine("================Order Details================");
+            foreach (LineItem item in items)
+            {
+                Console.WriteLine(item);
+                total+=item.ItemCost;
+            }
+            Console.WriteLine("================================");
+            Console.WriteLine("Total Cart Cost: "+ total);
+        }
+    }
+}
